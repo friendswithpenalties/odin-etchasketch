918754388f50f1ff0632a3677d56d2a470f96d98
friendswithpenalties <madonna.s.murphy@gmail.com>
2025-04-16 22:30:55 -0700

    Made a function that adjusts the grid size according to the number (n x n) and ALSO produce a row of squares by n number

--- javascript.js
+++ javascript.js
@@ -1,8 +1,5 @@
 
-
-
-
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
 
 function createSquareDiv() {
     const squareDiv = document.createElement("div");
@@ -10,3 +7,25 @@ function createSquareDiv() {
     container.appendChild(squareDiv);
     return squareDiv;
 }
+let n = 4;
+let gridSize = n * n;
+console.log(gridSize);
+/*
+function createGrid(n) {
+    let n = 10
+    for (let i = 0; i < n; i++) {
+        createSquareDiv();
+    }
+}
+*/
+for (let i = 0; i < gridSize; i++) {
+    createSquareDiv();
+}
+
+function resizeContainer() {
+    const gridSize = gridSize * 10;
+    container.style.width = gridSize + "px";
+    container.style.height = gridSize + "px";
+}
+resizeContainer();
+
