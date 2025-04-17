06b85556dba10c6e46ec8b8375db71195f4d21a6
friendswithpenalties <madonna.s.murphy@gmail.com>
2025-04-16 23:41:21 -0700

    Made the grid size to be fixed when making the webpage smaller

--- javascript.js
+++ javascript.js
@@ -1,31 +1,32 @@
 
-const container = document.querySelector(".container");
+// Produce a Grid with square boxes //
+
+const grid = document.querySelector(".grid");
 
 function createSquareDiv() {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square");
-    container.appendChild(squareDiv);
-    return squareDiv;
+    grid.appendChild(squareDiv);
 }
-let n = 4;
+
+let n = 13;
 let gridSize = n * n;
 console.log(gridSize);
-/*
-function createGrid(n) {
-    let n = 10
-    for (let i = 0; i < n; i++) {
-        createSquareDiv();
-    }
-}
-*/
+
 for (let i = 0; i < gridSize; i++) {
     createSquareDiv();
 }
 
-function resizeContainer() {
-    const grid = n * 10;
-    container.style.width = grid + "px";
-    container.style.height = grid + "px";
+function resizeContainer(n) {
+    const newGrid = n * 10;
+    grid.style.width = newGrid + "px";
+    grid.style.height = newGrid + "px";
+    grid.style.minWidth = newGrid + "px";
+    grid.style.minHeight = newGrid + "px";
 }
-resizeContainer();
+resizeContainer(n);
+
+// Setting up hover effect so grid divs change color //
+
+
 
