a601570238b90ea756d499700a1ff9a4165d463e
friendswithpenalties <madonna.s.murphy@gmail.com>
2025-04-23 21:11:06 -0700

    Was able to create new grid with new input number but now hover isn't working

--- javascript.js
+++ javascript.js
@@ -9,8 +9,6 @@ function createSquareDiv() {
     grid.appendChild(squareDiv);
 }
 
-let n = 2;
-
 function createGrid(n){
     let gridSize = n * n;
     console.log(gridSize);
@@ -19,44 +17,34 @@ function createGrid(n){
     }
     grid.style.width = n * 10 + "px";
     grid.style.height = n * 10 + "px";
-    grid.style.minWidth = n * 10+ "px";
+    grid.style.minWidth = n * 10 + "px";
     grid.style.minHeight = n * 10 + "px";
 }
-createGrid(n);
-
-/*function resizeContainer(x) {
-    const newGrid = x * 10;
-    grid.style.width = newGrid + "px";
-    grid.style.height = newGrid + "px";
-    grid.style.minWidth = newGrid + "px";
-    grid.style.minHeight = newGrid + "px";
-}
-resizeContainer(x); */
-
-// Setting up hover effect so grid divs change color //
-
-// try making new div for new color square?//
-
-const colorSquare = document.querySelectorAll(".square");
-
-for (const color of colorSquare) {
-    color.addEventListener("mouseover", () => {
-        color.style.backgroundColor = "white";
+function removeGridSquares() {
+    const gridSquares = document.querySelectorAll(".square");
+    gridSquares.forEach(square => {
+        square.parentNode.removeChild(square);
     });
 }
 
 // Create a pop up button that takes in the input of 
 // user for size of grid
 const inputbtn = document.querySelector(".inputbtn");
-function popUpInput() {
-    let input = prompt("Input your grid size:");
-    if (input > 100){
-        alert("Too large!");
-        setTimeout(window.location.reload(), 3000);
-    } else {
-        console.log(input);
-        createGrid(input);
-        //resizeContainer(input);
-    }
+inputbtn.addEventListener("click", () => {
+    n = prompt("Input new grid size:");
+    removeGridSquares();
+    createGrid(n);
+});
+
+window.onload = () => {
+    createGrid(4);
 }
-inputbtn.addEventListener("click", popUpInput);
+// Setting up hover effect so grid divs change color //
+// try making new div for new color square?//
+
+const colorSquare = document.querySelectorAll(".square");
+for (const color of colorSquare) {
+    color.addEventListener("mouseover", () => {
+        color.style.backgroundColor = "white";
+    });
+}
